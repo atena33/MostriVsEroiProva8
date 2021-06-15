47d6430831622348cd289ba52a4b00880256dbf6
Anna <annamura833@gmail.com>
2021-06-15 13:29:48 +0200

    Aggiungere i file di progetto.

---
+++ MostriVsEroi/MostriVsEroi/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MostriVsEroi
+{
+    public class Class1
+    {
+    }
+}
---
+++ MostriVsEroi/MostriVsEroiConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MostriVsEroiConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
